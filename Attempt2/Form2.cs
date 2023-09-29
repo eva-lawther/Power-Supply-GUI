b006ae02c111fac3e5620418f1d73cb9f8458a43
EvaLawther <elawther@slipstream-design.co.uk>
2023-09-29 15:28:17 +0100

    all pop ups work

--- Attempt2/Form2.cs
+++ Attempt2/Form2.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Management.Automation.Language;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,10 @@ using Microsoft.PowerShell.Commands;
 using Microsoft.Scripting.Hosting.Shell;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
 using Button = System.Windows.Forms.Button;
+using Label = System.Windows.Forms.Label;
+using TextBox = System.Windows.Forms.TextBox;
 
 namespace Attempt2
 {
@@ -110,21 +114,28 @@ namespace Attempt2
                     
                 }
                 string work = commandString.Split(';')[0];
-                string final = fillInCommands(work);
-                writeToFile("commandList.txt", final);
+                string[] commandArray = fillInCommands(work);
+                
+                //string final = fillInCommands(work);
+                //writeToFile("commandList.txt", final);
             }
         }
         // gets name of chosen command withn category and writes machine instruction to file
 
-        private string fillInCommands(string command)
+
+
+        private string[] fillInCommands(string command)
         {
             string[] commandString = command.Split('<','>');
             (string[,] inputArray, string[] key) = fillInInputArray();
             int tag = 0;
-
+            string[] commandArray = new string[commandString.Length];
+            Button button = new Button();
+            button.Text = "okay";
             foreach (string element in commandString)
             {
-                tag ++;
+                string chosen = element;
+                Console.WriteLine(chosen);
                 if (element != " ")
                 {
                     
@@ -141,59 +152,148 @@ namespace Attempt2
                     }
                     if (found)
                     {
-                        int chosen = makePopUp(inputArray[i, 1], inputArray[i, 2], tag);
+                        chosen =  makePopUp(inputArray[i, 1], inputArray[i, 2], tag);
+                        //chosen = null;//button.Text;
                     }
                 }
+                commandArray[tag] = chosen;
+                tag++;
+
+
             }
-            return command;
+            //string commandFinal = string.Join("", commandArray);
+            //Console.WriteLine(commandFinal);
+
+            return commandArray;
         } 
         // breaks command into what needs user value and makes calls pop up 
 
-        private int makePopUp(string values, string request,int tag) 
+        private string makePopUp(string values, string request,int tag) 
         {
             Font font = new Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular);
             Label label = new Label { AutoSize = true, Font = font, Tag = tag  };
             label.Text = request;
             label.Location = new Point(30, 34);
-            if (values.Substring(0,1) == "[")
-            {
-                values = values.Substring(1, values.Length-2);
-                string[] toReplace = values.Split(',');
-                CheckedListBox options = new CheckedListBox { FormattingEnabled = true, Tag = tag + "1" };
-                options.SelectedIndexChanged += new System.EventHandler(getChoice);
-                Button button = new Button { Tag = tag };
-                button.Location = new Point(30, 80);
-                button.Text = "Okay";
-                button.Click += new System.EventHandler(getChosen);
-                foreach (string value in toReplace)
-                {
-                    options.Items.Add(value);
-                }
-                options.Location = new Point(30, 80);
-                //using (Form form = new Form())
-                
+            Button button = new Button { Tag = tag };
+            button.Text = "Okay";
 
-                
-                Form form = new Form();
-                form.Text = "Request!";
-                form.Controls.Add(button);
-                form.Controls.Add(label);
-                form.Controls.Add(options);
-                label.Visible= true;
-                form.Show();
-            }
-
+            if (values.Substring(0,1) == "[") {checkListPopUp(label, tag, values, button);}
+            else if (values == "0-49") {stringEnterPopUp(label,tag, button);}
+            else { decimalEnterPopUp() ; }
             
-            return 0;
+            return "ugh"; // needs to be chosen value from popup
         }
+
+        private void decimalEnterPopUp(Label label, int tag, Button button)
+        {
+            TextBox textBox = new TextBox { Tag = tag + "1" };
+            textBox.Location = new Point(30, 50);
+            button.Location = new Point(30, 80);
+            button.Click += new System.EventHandler(setValue);
+
+            Form form = new Form { Tag = tag };
+            form.Text = "Request!";
+            form.Controls.Add(button);
+            form.Controls.Add(label);
+            form.Controls.Add(textBox);
+            //label.Visible = true;
+            form.Show();
+        }
+
+        private void setValue(object sender, EventArgs e)
+        {
+            try
+            {
+                Button button = (Button)sender;
+                Control parent = button.Parent;
+                string tag = button.Tag.ToString();
+                TextBox textbox = (TextBox)findTextFromTag(parent, tag + "1");
+                string choice = textbox.Text;
+                Form form = (Form)parent;
+                form.Hide();
+                //if (!int.TryParse(choice, out _)) { throw new Exception(); } //needs to be string not int
+
+
+            }
+            catch { MessageBox.Show("please write a number between 1 and 49"); }
+        }
+
         // makes pop up form
+
+
+
+        private void stringEnterPopUp(Label label, int tag, Button button)
+        {
+            TextBox textBox = new TextBox { Tag = tag + "1" };
+            textBox.Location = new Point(30, 50);
+            button.Location = new Point(30, 80);
+            button.Click += new System.EventHandler(setNumber);
+
+            Form form = new Form { Tag = tag };
+            form.Text = "Request!";
+            form.Controls.Add(button);
+            form.Controls.Add(label);
+            form.Controls.Add(textBox);
+            //label.Visible = true;
+            form.Show();
+
+        }
+
+        private void setNumber(object sender, EventArgs e)
+        {
+            try
+            {
+                Button button = (Button)sender;
+                Control parent = button.Parent;
+                string tag = button.Tag.ToString();
+                TextBox textbox = (TextBox)findTextFromTag(parent, tag + "1");
+                string choice = textbox.Text;
+                Form form = (Form)parent;
+                form.Hide();
+                if (!int.TryParse(choice, out _)) { throw new Exception(); }
+
+
+            }
+            catch { MessageBox.Show("please write a number between 1 and 49"); }
+            
+            //button.Text = choice;
+            //return choice;
+        }
+
+        
+        private void checkListPopUp(Label label, int tag, string values, Button button)
+        {
+            
+            values = values.Substring(1, values.Length - 2);
+            string[] toReplace = values.Split(',');
+            CheckedListBox options = new CheckedListBox { FormattingEnabled = true, Tag = tag + "1" };
+            options.SelectedIndexChanged += new System.EventHandler(onlyOneBox);
+
+            button.Location = new Point(30, 150);
+            button.Click += new System.EventHandler(okayPressed);
+            foreach (string value in toReplace)
+            {
+                options.Items.Add(value);
+            }
+            options.Location = new Point(30, 80);
+            //using (Form form = new Form())
+
+
+
+            Form form = new Form { Tag = tag };
+            form.Text = "Request!";
+            form.Controls.Add(button);
+            form.Controls.Add(label);
+            form.Controls.Add(options);
+            //label.Visible = true;
+            form.Show();
+        }
 
         private Control findTextFromTag(Control parentControl, string tagString)
         {
             foreach (Control text in parentControl.Controls)
             {
                 string controlTag = text.Tag.ToString();
-                Console.WriteLine(controlTag);
                 if (controlTag != null && controlTag == tagString)// Contains(tagString))
                 {
                     return text;
@@ -202,7 +302,7 @@ namespace Attempt2
             return null;
         }
 
-        private void getChoice(object sender, EventArgs e)
+        private void onlyOneBox(object sender, EventArgs e)
         {
             CheckedListBox source = (CheckedListBox)sender;
             int index = source.SelectedIndex;
@@ -215,25 +315,29 @@ namespace Attempt2
                     source.SetItemChecked(i, false);
                 }
             }
-            string choice = source.CheckedItems.ToString();
-            Console.WriteLine(choice);
+            //string choice = source.CheckedItems.ToString();
+            //Console.WriteLine(choice);
         }
+        // Makes sure only one box is chosen in checkListBox
 
-        private void getChosen(object sender, EventArgs e)
+        private void okayPressed(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             Control parent = button.Parent;
             string tag = button.Tag.ToString();
-            string choice = getNumber(tag, parent);
-            
+            string choice = getChoice(tag, parent);
+            //button.Text = choice;
             //return choice;
         }
 
-        private string getNumber(string tag, Control parent)
+        private string getChoice(string tag, Control parent) //gets choice and closes form box
         {
             CheckedListBox checkedList = (CheckedListBox)findTextFromTag(parent, tag + "1");
             string choice = checkedList.CheckedItems[0].ToString();
             Console.WriteLine($" Choice: {choice}");
+            Console.WriteLine(tag);
+            Form form = (Form)parent;
+            form.Hide();
             return choice;
         }
            
